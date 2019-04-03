ca39bbdec5a5b3c940e2e2161bf9366b6c1fcbde
Tom Tarpey <tomtarpeydev@gmail.com>
2019-04-03 19:14:04 +0100

    finished the nieve version of answer 1 for datatypes.py using a variable to store the value

--- src/datatypes.py
+++ src/datatypes.py
@@ -14,7 +14,9 @@ y = "7"
 # Write a print statement that combines x + y into the integer value 12
 
 # YOUR CODE HERE
-z = x + int(y)
+z = x + int(y) # ==> z = 12
+print(z) # ==> 12
+
 
 # Write a print statement that combines x + y into the string value 57
 
